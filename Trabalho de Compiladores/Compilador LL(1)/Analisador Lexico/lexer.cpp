e02d51e6861df7e46dd8226ba2c1327295ea6b39
José Uchoa <57728891+JosephUchoa@users.noreply.github.com>
2021-12-14 09:29:28 -0300

    correção

---
+++ Trabalho de Compiladores/Compilador LL(1)/Analisador Lexico/lexer.cpp
@@ -0,0 +1,12 @@
+#include "tokens.cpp"
+#include<vector>
+#include<utility>
+
+class lexer {
+    private:
+        std::vector<std::pair<std::string, std::string>> tokens;
+    public:
+        bool lex(std::string) {
+            std::string temp;
+        }
+};
